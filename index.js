36ab8b133032a0216ed4c1eb9f8b1fa824456112
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-03-07 10:55:24 -0300

    feat: adding express module

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const http = require('http');
+const express = require('http');
 
-let server = http.createServer((req, res) => {
-    console.log("URL:", req.url);
-    console.log("METHOD:", req.method);
-})
+const app = express();
 
-server.listen(3000, "127.0.0.1", () => {
+app.get("/", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html")
+    res.end("<h1>Hello World</h1>");
+});
+
+app.get("/users", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json")
+    res.json({
+        users: "Miguel",
+        email: "email@provider.com",
+        id: 1
+    });
+});
+
+app.listen(3000, "127.0.0.1", () => {
     console.log("Server is running");
 })
