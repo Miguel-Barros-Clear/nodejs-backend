908771e0ec4c31ef30d8ad1ba562130d56d86cce
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-03-07 11:09:40 -0300

    perf: splitting route files

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const express = require('http');
+const express = require('express');
+let routesIndex = require('./routes/index')
+let routesUsers = require('./routes/users')
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html")
-    res.end("<h1>Hello World</h1>");
-});
-
-app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json")
-    res.json({
-        users: "Miguel",
-        email: "email@provider.com",
-        id: 1
-    });
-});
+app.use(routesIndex);
+app.use("/users", routesUsers);
 
 app.listen(3000, "127.0.0.1", () => {
     console.log("Server is running");
