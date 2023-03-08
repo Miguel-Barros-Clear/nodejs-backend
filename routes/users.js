812ff9b931e7342b87e83f4dfbb35d053c2563d6
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-03-08 08:00:38 -0300

    feat: adding post in users route

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,38 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
+
 module.exports = (app) => {
     app.get("/users", (req, res) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json")
-        res.json({
-            users: [{
-                name: "Miguel",
-                email: "email@provider.com",
-                id: 1
-            }]
-        });
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+            if (errbody) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json")
+                res.json({
+                    users
+                });
+            }
+        })
     });
 
-    app.get("/users/admin", (req, res) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json")
-        res.json({
-            users: []
-        });
-    });
+    app.post("/users", (req, res) => {
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                console.log(req.body);
+                res.status(200).json(user);
+            }
+        })
+    })
 };
